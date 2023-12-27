e297b62d808fd1e430366a303b799b515c918686
zzzzseong <jung48182@gmail.com>
2023-12-27 10:49:37 +0900

    feat(Board): Board 도메인 Entity 레벨 클래스 추가

--- src/main/java/sparta/a08/trello/board/entity/Board.java
+++ src/main/java/sparta/a08/trello/board/entity/Board.java
@@ -1,14 +1,40 @@
 package sparta.a08.trello.board.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sparta.a08.trello.common.BaseEntity;
 
 @Entity
-public class Board {
+@Getter
+@Table(name = "board")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends BaseEntity {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_id")
     private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "color", nullable = false)
+    private String colorURL;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "visibility", nullable = false)
+    private BoardAccess visibility;
+
+    @Builder
+    public Board(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.colorURL = BoardColor.BLACK.getUrl();
+        this.visibility = BoardAccess.PRIVATE;
+    }
 }
---
+++ src/main/java/sparta/a08/trello/board/entity/BoardAccess.java
@@ -0,0 +1,9 @@
+package sparta.a08.trello.board.entity;
+
+/**
+ * 보드 접근 레벨을 정의하고 있는 Enum class
+ * 기본 접근 레벨은 PRIVATE로 개발
+ * */
+public enum BoardAccess {
+    PRIVATE, PUBLIC
+}
