84030fbc328d199f1e5f375239c61193893be8e8
zzzzseong <jung48182@gmail.com>
2023-12-28 15:58:53 +0900

    test(Board): Board 수정 성공 Controller 테스트 추가

--- src/test/java/sparta/a08/trello/board/controller/BoardControllerTest.java
+++ src/test/java/sparta/a08/trello/board/controller/BoardControllerTest.java
@@ -16,6 +16,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,11 +29,11 @@ class BoardControllerTest extends ControllerTest implements BoardTest {
     BoardServiceImpl boardService;
 
     @Nested
-    @DisplayName("Board 생성 요청")
+    @DisplayName("Board 생성 요청 테스트")
     class createBoardTest {
 
         @Test
-        @DisplayName("Board 생성 요청 성공")
+        @DisplayName("Board 생성 요청 테스트 성공")
         void createBoard_success() throws Exception {
             //given
             given(boardService.createBoard(eq(TEST_USER), any(BoardRequest.class))).willReturn(TEST_BOARD_RESPONSE);
@@ -41,8 +42,7 @@ class BoardControllerTest extends ControllerTest implements BoardTest {
             ResultActions action = mockMvc.perform(post("/api/board")
                     .contentType(MediaType.APPLICATION_JSON)
                     .accept(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(TEST_BOARD_REQUEST))
-            );
+                    .content(objectMapper.writeValueAsString(TEST_BOARD_REQUEST)));
 
             //then
             action
@@ -52,7 +52,7 @@ class BoardControllerTest extends ControllerTest implements BoardTest {
         }
 
         @Test
-        @DisplayName("Board 생성 요청 실패 - 제목이 없는 경우")
+        @DisplayName("Board 생성 요청 테스트 실패 - 제목이 없는 경우")
         void createBoard_fail_invalidTitle() throws Exception {
             //given
             BoardRequest request = BoardRequest.builder()
@@ -64,8 +64,7 @@ class BoardControllerTest extends ControllerTest implements BoardTest {
             ResultActions action = mockMvc.perform(post("/api/board")
                     .contentType(MediaType.APPLICATION_JSON)
                     .accept(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(request))
-            );
+                    .content(objectMapper.writeValueAsString(request)));
 
             //then
             action
@@ -73,4 +72,32 @@ class BoardControllerTest extends ControllerTest implements BoardTest {
                     .andExpect(result -> assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException));
         }
     }
+
+    @Nested
+    @DisplayName("Board 수정 요청 테스트")
+    class updateBoardTest {
+
+        @Test
+        @DisplayName("Board 수정 요청 테스트 성공")
+        void updateBoard_success() throws Exception {
+            //given
+            given(boardService.updateBoard(eq(TEST_USER), any(BoardRequest.class), eq(TEST_BOARD_ID)))
+                    .willReturn(TEST_BOARD_RESPONSE);
+
+            //when
+            ResultActions action = mockMvc.perform(patch("/api/board/{boardId}", TEST_BOARD_ID)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(TEST_BOARD_REQUEST)));
+
+            //then
+            action
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.title").value(TEST_BOARD_REQUEST.getTitle()))
+                    .andExpect(jsonPath("$.content").value(TEST_BOARD_REQUEST.getContent()));
+        }
+
+
+    }
+
 }
